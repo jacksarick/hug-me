239af844ebd24d634c202250d134980947582e7d
Jack Sarick <jacksarick@gmail.com>
2016-10-15 19:41:10 -0400

    Save Sat 15 Oct 2016 19:41:10 EDT

---
+++ test.js
@@ -0,0 +1,8 @@
+var SerialPort = require("serialport");
+var port = new SerialPort("/dev/cu.wchusbserial1420", {
+	baudRate: 9600
+});
+
+port.on('data', function (data) {
+	console.log(data + []);
+});
