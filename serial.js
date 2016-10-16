458ba4f5c0ebccad986f552dcc835ab189ff085c
Jack Sarick <jacksarick@gmail.com>
2016-10-16 10:45:32 -0400

    Tested. Works

--- serial.js
+++ serial.js
@@ -6,15 +6,17 @@ const blu_port = 'cu.HC-06-DevB' //Bluetooth
 const usb_port = 'cu.wchusbserial1420' //USB
 const ports = [fs.readdirSync("/dev").indexOf(blu_port) > 0, fs.readdirSync("/dev").indexOf(usb_port) > 0]
 
+console.log(ports);
 if(ports[0] || ports[1]) {
 
 	port = blu_port;
 	if (!ports[0]){
 		port = usb_port;
 	}
+	console.log(port);
 
 	serialjs.open(
-		port
+		"/dev/" + port,
 		start,
 		'\n'
 	);
@@ -25,11 +27,10 @@ if(ports[0] || ports[1]) {
 		);
 	}
 
-	var target = $("#target");
 	function recieved(data){
+		console.log(data);
 		state = (((data + []).split(" ").reduce((a, b) => a + b, 0)) > 0);
 
-		target.html(state + []);
 		if (state) {
 			$.post("https://sarick.tech:3000", {function: "log-data", user: usersettings.user, pass: usersettings.pass, plush: 3, date: Math.floor(new Date() / 1000)}, function(data) {
 				console.log(data)
